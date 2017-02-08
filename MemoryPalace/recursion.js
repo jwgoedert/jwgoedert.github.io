2588afe13b387f573e25d55195b45c07e460fd69
James Wesley Goedert <jwgoedert@gmail.com>
2017-02-08 11:46:38 -0600

    stringified message with result for factorial

--- MemoryPalace/recursion.js
+++ MemoryPalace/recursion.js
@@ -6,4 +6,5 @@ let factorial = (num)=>{
 	if(num === 0)return 1;
 	return num * factorial(num - 1);
 }
-console.log(factorial(4));
+let num = 4;
+console.log(`the factorial of ${num} is ${factorial(4)}`);
