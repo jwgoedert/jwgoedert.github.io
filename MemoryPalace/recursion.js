d324074fb2037e110b120431a065c321de4bdd07
James Wesley Goedert <jwgoedert@gmail.com>
2017-02-08 11:43:50 -0600

    added factorial

--- MemoryPalace/recursion.js
+++ MemoryPalace/recursion.js
@@ -1,1 +1,9 @@
 console.log("this is working");
+
+//RECURSION-NIOSRUCER-- recursion practices from ali's git
+//Factorial- takes a number and returns the factorial of that number
+let factorial = (num)=>{
+	if(num === 0)return 1;
+	return num * factorial(num - 1);
+}
+console.log(factorial(4));
