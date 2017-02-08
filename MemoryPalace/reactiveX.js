373e97bb523a330cec8e5d59068a48bc53ed99b0
James Wesley Goedert <jwgoedert@gmail.com>
2017-02-08 13:07:37 -0600

    fixed reactiveX

--- MemoryPalace/reactiveX.js
+++ MemoryPalace/reactiveX.js
@@ -1,1 +1,20 @@
-reactiveX.js
+
+
+
+ return movieLists.map(function(genre){
+    	return genre.videos.map(function(videos){
+        return video.map(function(video){
+          return {'id': video.id,
+                  'title': video.title,
+                  'boxart':video.boxarts.filter(function(boxart){
+                    	return (boxart.width === 150 && height === 200);
+                  }),
+          		
+          }
+        });
+ 
+      });
+  
+  }).concatAll(); // Complete this expression!
+
+}
